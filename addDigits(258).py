766302afb7fea0233b8f50a1c851e1d5bc71805a
Srilaya_Putta <puttasrilaya15@gmail.com>
2026-04-20 18:17:11 +0530

    Add addDigits method to Solution class
    Implement addDigits method to sum the digits of a number until a single digit is obtained.

---
+++ addDigits(258).py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num>9:
+            temp=0
+            while num>0:
+                temp+=num%10
+                num=num//10
+            num=temp
+        return num
+            
+        
+
+
+
+        
