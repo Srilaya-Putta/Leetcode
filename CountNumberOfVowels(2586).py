b8467ad3102359c09f063253dcc909535c04ab9c
Srilaya_Putta <puttasrilaya15@gmail.com>
2026-04-27 18:51:12 +0530

    Create CountNumberOfVowels(2586).py

---
+++ CountNumberOfVowels(2586).py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def vowelStrings(self, words, left, right):
+        """
+        :type words: List[str]
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        v=['a','e','i','o','u']
+        c=0
+        for i in range(left,right+1):
+            word=words[i]
+            if word[0] in v and word[-1] in v:
+                c+=1
+        return c
+        
