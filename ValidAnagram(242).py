e4a1eefeaf81639a5c99f3a68998e440478577d0
Srilaya_Putta <puttasrilaya15@gmail.com>
2026-04-28 20:03:58 +0530

    Implement isAnagram method to check for anagrams

---
+++ ValidAnagram(242).py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        c=[0]*26
+        for char in s:
+            c[ord(char)-ord('a')]+=1
+        for char in t:
+            if c[ord(char)-ord('a')]==0:
+                return False
+            c[ord(char)-ord('a')]-=1
+        return True
+        
